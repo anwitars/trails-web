f782b7ca954325b600b99a123fb49f0acce43851
anwitars <maksa22242@gmail.com>
2025-08-09 17:03:16 +0200

    enh: general peek page for searching

--- src/app/peek/page.tsx
+++ src/app/peek/page.tsx
@@ -8,17 +8,17 @@ export default function Page() {
   const router = useRouter();
 
   const onFound = useCallback(
-    (trailId: string) => router.push(`/info/${trailId}`),
+    (trailId: string) => router.push(`/peek/${trailId}`),
     [router],
   );
 
   return (
     <div className="page">
-      <h1 className="mb-8">Trail Info</h1>
+      <h1 className="mb-8">Peek Trail</h1>
       <TrailSearcher
-        buttonLabel="Get Info"
+        buttonLabel="Peek Trail"
         onFoundAction={onFound}
-        apiEndpoint="/info/{trail_id}"
+        apiEndpoint="/peek/{trail_id}"
       />
     </div>
   );
---
+++ src/components/TrailSearcher.tsx
@@ -0,0 +1,77 @@
+"use client";
+
+import { useForm } from "@/form";
+import { LabeledTextInput } from "./TextInput";
+import * as z from "zod";
+import { useState } from "react";
+import { useSafeCallback } from "./ErrorBoundary";
+import { apiSend } from "@/api/utils";
+
+const TrailSearchSchema = z.object({
+  trailId: z.string(),
+});
+
+type TrailSearcherProps = {
+  buttonLabel: string;
+  onFoundAction: (trail: string) => void;
+  apiEndpoint: "/info/{trail_id}" | "/peek/{trail_id}";
+};
+
+export const TrailSearcher = ({
+  buttonLabel,
+  onFoundAction,
+  apiEndpoint,
+}: TrailSearcherProps) => {
+  const form = useForm(TrailSearchSchema, { trailId: "" });
+  const [searching, setSearching] = useState(false);
+  const [backendErrors, setBackendErrors] = useState<string[]>([]);
+
+  const handleSearch = useSafeCallback(async () => {
+    setSearching(true);
+    setBackendErrors([]);
+
+    const response = await apiSend(apiEndpoint, "get", {
+      pathParams: { trail_id: form.values.trailId },
+      gracefulNotFound: true,
+    });
+
+    switch (response.code) {
+      case 200: {
+        onFoundAction(form.values.trailId);
+        break;
+      }
+      case 404: {
+        setBackendErrors(["Trail not found"]);
+        break;
+      }
+      case 422: {
+        setBackendErrors(
+          response.data.detail.map((e: { msg: string }) => e.msg),
+        );
+        break;
+      }
+    }
+
+    setSearching(false);
+  }, []);
+
+  return (
+    <form className="card">
+      <LabeledTextInput
+        label="Trail ID"
+        value={form.values.trailId}
+        onChange={(trailId) => form.setValues({ trailId })}
+        errors={backendErrors}
+      />
+      <div className="flex justify-end">
+        <button
+          className="button"
+          onClick={handleSearch}
+          disabled={!form.hasChanged || searching}
+        >
+          {buttonLabel}
+        </button>
+      </div>
+    </form>
+  );
+};
